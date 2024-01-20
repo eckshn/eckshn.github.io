eff3059870cd43a812fabc5130a86c24e19f7904
Eckart Schneider <eckshn@Eckarts-Air.fios-router.home>
2024-01-20 14:33:04 -0500

    create Profile Card

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <ProfileCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+import ProfileCard from './components/ProfileCard.vue';
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    Home,
+    ProfileCard
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ portfolio/src/components/ProfileCard.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="main-container">
+        <div class="container-one">
+            <div class="profile">
+                <img src="../assets/logo.png" alt="">
+                <h3>Eckart Schneider</h3>
+                <p>Software Developer</p>
+            </div>
+            <div class="socials">
+                <a href="">
+                    <div class="link">
+                        <img src="../assets/logo.png" alt="">
+                        <h2>GitHub</h2>
+                    </div>
+                    <div class="link">
+                        <img src="../assets/logo.png" alt="">
+                        <h2>LinkedIn</h2>
+                    </div>
+                    <div class="link">
+                        <img src="../assets/logo.png" alt="">
+                        <h2>Email</h2>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="container-two">
+            <div class="about-me">
+                <h1>About Me</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Sit nemo repudiandae odit omnis voluptatum et in sunt minus animi, 
+                    eum atque enim temporibus nostrum eos accusamus molestiae ipsum quo voluptas.</p>
+            </div>
+            <div class="skills">
+                <h1>Skills</h1>
+
+            </div>
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style scoped>
+    img {
+        height: 20px;
+    }
+
+    .main-container {
+        display: flex;
+        flex-direction: row;
+        background-color:cadetblue;
+    }
+
+    .container-one {
+        background-color:steelblue;
+        width: 15%
+    }
+
+    .container-two {
+        width: 85%;
+    }
+</style>
